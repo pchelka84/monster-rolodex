a3fbc7209776c2d192f981fcd08d326d9aac63aa
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-08-02 15:38:54 -0400

    Fetch monsters

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => console.log(this.setState({ monsters: users })));
+  }
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
   }
 }
 
